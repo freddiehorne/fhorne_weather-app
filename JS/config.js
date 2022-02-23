0aad0bd0239845dea226c576013725a2bafdebdb
Frederick Horne <frederickhorne@Fredericks-MacBook-Pro.local>
2022-02-23 16:25:06 +0000

    I have added comments to the code (except CSS) explaining what everything does

--- JS/config.js
+++ JS/config.js
@@ -1,7 +1,9 @@
+//Open Weather Map API URL
 export function getAPI_URL(value) {
 	return `https://api.openweathermap.org/data/2.5/forecast?q=${value}&appid=24581586ed3d69ed3183980de6c64e19`;
 }
 
+//Template for the main HTML to be inserted into the DOM
 export function weatherHTML(timeSlot) {
 	return `
 				<h3>${Math.round(timeSlot.main.temp - 273.15)}°C</h3>
--- JS/weather.js
+++ JS/weather.js
@@ -1,20 +1,24 @@
 import { weatherHTML, getAPI_URL } from "./config.js";
 
+//Assign variables to HTML elements
 const place = document.getElementById("place");
 const button = document.getElementById("button");
 const weatherContainer = document.getElementById("weatherContainer");
 let weatherData;
 
+//Run getData function on button click
 button.addEventListener("click", () => {
 	getData();
 });
 
+//Run getData function with ENTER key
 place.addEventListener("keydown", (e) => {
 	if (e.keyCode === 13) {
 		getData();
 	}
 });
 
+//Retrieve data from API URL and save into a variable
 async function getData() {
 	try {
 		const result = await axios.get(getAPI_URL(place.value));
@@ -25,11 +29,12 @@ async function getData() {
 	}
 }
 
+//For loop to determine number of times we access the API data. It is also used loop ove timeArr in order to make dynamic times and then insert the HTML into the DOM
 function onWeatherData(weatherData) {
-	const timeArr = ["Now", "In 3 Hours", "In 6 Hours"];
+	const timeArr = ["Now", "In 3 Hours", "In 6 Hours", "In 9 Hours"];
 	let holder = "";
-	for (let i = 0; i < timeArr.length; i++) {
-		const el = timeArr[i];
+	for (let i = 0; i < 4; i++) {
+		timeArr[i];
 		holder += `<div id="holder${i}" class="holder">
 						<h2>${timeArr[i]}</h2>
 						${weatherHTML(weatherData[i])}
